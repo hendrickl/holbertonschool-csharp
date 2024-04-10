2219061315398d8bb302dc31cb426bba9ec151e3
hendrickl <h.lincertin@gmail.com>
2024-04-10 16:16:38 +0200

    Task 6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                Console.Write(i);
+
+                if (j == 9 && i == 8)
+                {
+                    Console.Write(j);
+                    break;
+                }
+
+                Console.Write(j + ", ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
