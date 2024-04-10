67e8b76447b297571cd7ab6ab032b7ca74a872d0
hendrickl <h.lincertin@gmail.com>
2024-04-10 16:52:58 +0200

    Task 7

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (c.ToString() == "a" || c.ToString() == "h" || c.ToString() == "g")
+        {
+            return true;
+        }
+        return false;
+    }
+}
