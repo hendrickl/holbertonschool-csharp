2cd6cb8fa63b0ec14e927a51e589890094b85c54
hendrickl <h.lincertin@gmail.com>
2024-04-10 15:58:35 +0200

    Task 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write("{0:D2}, ", i);
+        }
+    }
+}
