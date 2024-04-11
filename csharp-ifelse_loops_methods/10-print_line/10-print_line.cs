a5c107439bad6e9ec9fa921f8f7187700a9c418e
hendrickl <h.lincertin@gmail.com>
2024-04-11 10:43:43 +0200

    Task 10

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        string line = "";
+        for (int i = 0; i < length; i++)
+        {
+            line += "-";
+        }
+        Console.WriteLine(line);
+    }
+}
