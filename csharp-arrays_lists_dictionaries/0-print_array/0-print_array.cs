a48739ba2fd637ce64fddfc6e2f9c0456c949092
hendrickl <h.lincertin@gmail.com>
2024-04-11 15:55:14 +0200

    Task 0

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -10,20 +10,24 @@ class Array
             return null;
         }
 
-        int[] newArray = new int[size];
-
         if (size == 0)
         {
             Console.WriteLine("");
             return new int[0];
         }
 
+        int[] newArray = new int[size];
+
         for (int i = 0; i < size; i++)
         {
             newArray[i] = i;
+            if (i == size - 1)
+            {
+                Console.WriteLine(i);
+                break;
+            }
             Console.Write($"{i} ");
         }
-        Console.WriteLine("");
         return newArray;
     }
 }
