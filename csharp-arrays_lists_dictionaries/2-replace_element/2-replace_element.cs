37200d0fc112dd12ca6d1384b185c1befac6daed
hendrickl <h.lincertin@gmail.com>
2024-04-11 17:28:25 +0200

    Task 2

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Array
+{
+    public static void ReplaceElement(int[] array, int index, int n)
+    {
+        if (index >= array.Length || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return;
+        }
+        array[index] = n;
+    }
+}
