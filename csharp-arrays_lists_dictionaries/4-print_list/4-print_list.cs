d0c490f231a139dfd828c6ca05b826cd20af3494
hendrickl <h.lincertin@gmail.com>
2024-04-11 18:02:06 +0200

    Task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> newList = new List<int>();
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        if (size == 0)
+        {
+            Console.WriteLine("");
+            return newList;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            newList.Add(i);
+            if (i == size - 1)
+            {
+                Console.WriteLine(i);
+                break;
+            }
+            Console.Write(i + " ");
+        }
+        return newList;
+    }
+}
