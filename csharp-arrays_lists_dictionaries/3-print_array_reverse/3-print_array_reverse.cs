7669925bb3274873c97bbfec58666b592faadf35
hendrickl <h.lincertin@gmail.com>
2024-04-11 17:47:16 +0200

    Task 3

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine("");
+            return;
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                Console.WriteLine(array[i]);
+                break;
+            }
+            Console.Write(array[i] + " ");
+        }
+    }
+}
