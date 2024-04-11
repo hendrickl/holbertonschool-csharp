2e8b4d21901d8846203ad542a61241c085560d54
hendrickl <h.lincertin@gmail.com>
2024-04-11 14:07:35 +0200

    Task 0

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        int[] newArray = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            if (size == 0)
+            {
+                Console.WriteLine("");
+            }
+            else if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            else
+            {
+                newArray[i] = i;
+                Console.Write($"{i} ");
+            }
+        }
+        return newArray;
+    }
+}
