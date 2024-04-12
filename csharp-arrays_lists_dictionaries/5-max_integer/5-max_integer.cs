f8ded7da2e0a22b777988426ffecf942892730af
hendrickl <h.lincertin@gmail.com>
2024-04-12 10:18:20 +0200

    Task 5

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        int maxInt = myList[0];
+
+        foreach (int number in myList)
+        {
+            if (number > maxInt)
+            {
+                maxInt = number;
+            }
+        }
+        return maxInt;
+    }
+}
