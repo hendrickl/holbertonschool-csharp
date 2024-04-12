3c15e217b7ff585257aeb9fd2d372485ea2aabe3
hendrickl <h.lincertin@gmail.com>
2024-04-12 14:59:38 +0200

    Task 13

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string biggestIntKey = "";
+        int biggestIntValue = 0;
+
+        foreach (KeyValuePair<string, int> entry in myList)
+        {
+            if (entry.Value > biggestIntValue)
+            {
+                biggestIntKey = entry.Key;
+                biggestIntValue = entry.Value;
+            }
+        }
+
+        return biggestIntKey;
+    }
+}
