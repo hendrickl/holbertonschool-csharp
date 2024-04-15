eecad8280c7bec8410ba1fffc503ddf0840e2682
hendrickl <h.lincertin@gmail.com>
2024-04-15 14:13:51 +0200

    Task 1

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+
+        List<int> commonSortedElements = new List<int>();
+
+        foreach (int element in set1)
+        {
+            if (set2.Contains(element))
+            {
+                commonSortedElements.Add(element);
+            }
+        }
+
+        return commonSortedElements;
+    }
+}
