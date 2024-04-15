6d4c013e4c869e210d2ebda5bf57b17b38676de2
hendrickl <h.lincertin@gmail.com>
2024-04-15 14:01:49 +0200

    Task 0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> uniqueIntegers = new HashSet<int>();
+        int sum = 0;
+
+        foreach (int number in myList)
+        {
+            if (uniqueIntegers.Add(number))
+            {
+                sum += number;
+            }
+        }
+
+        return sum;
+    }
+}
