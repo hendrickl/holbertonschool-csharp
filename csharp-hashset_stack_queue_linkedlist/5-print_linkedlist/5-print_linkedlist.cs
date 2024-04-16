5ea2e789aac1cb72c738c6104ed3767e26a11f7b
hendrickl <h.lincertin@gmail.com>
2024-04-16 11:24:45 +0200

    Task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> lList = new LinkedList<int>();
+
+        if (size < 0)
+        {
+            return lList;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            lList.AddLast(i);
+            Console.WriteLine(i);
+        }
+
+        return lList;
+    }
+}
