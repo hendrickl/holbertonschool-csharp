660de83e53373deb784d842e969603b5bef7eb04
hendrickl <h.lincertin@gmail.com>
2024-04-16 11:05:52 +0200

    Task 4: remove comments

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -5,14 +5,7 @@ class MyQueue
 {
     public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
     {
-        // Print the number of items in aQueue
-        // Format: Number of items: <number>
-
         Console.WriteLine($"Number of items: {aQueue.Count}");
-
-        // Print the item at the top of aQueue without removing it
-        // Format: First item: <item>
-        // If aQueue is empty, print Queue is empty
 
         if (aQueue.Count == 0)
         {
@@ -23,12 +16,7 @@ class MyQueue
             Console.WriteLine($"First item: {aQueue.Peek()}");
         }
 
-        // Add a new given item newItem to aQueue
-
         aQueue.Enqueue(newItem);
-
-        // Print if aQueue contains a given item search
-        // Format: Queue contains <search>: <True / False>
 
         if (aQueue.Contains(search))
         {
@@ -38,8 +26,6 @@ class MyQueue
         {
             Console.WriteLine($"Queue contains \"{search}\": False");
         }
-
-        // If aQueue contains the given item search, remove all items up to and including search; otherwise, leave aQueue as is
 
         if (aQueue.Contains(search))
         {
