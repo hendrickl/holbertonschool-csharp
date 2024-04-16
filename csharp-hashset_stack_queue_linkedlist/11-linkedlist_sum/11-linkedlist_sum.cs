a4aafcc9c1e725e85911a0b2073795d7e679b1b6
hendrickl <h.lincertin@gmail.com>
2024-04-16 13:24:05 +0200

    Task 11

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+
+        foreach (int node in myLList)
+        {
+            sum += node;
+        }
+
+        return sum;
+    }
+}
