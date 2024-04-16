dcccb0f46fd65909321802908563faf1e10c0d8c
hendrickl <h.lincertin@gmail.com>
2024-04-16 13:01:04 +0200

    Task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int index = 0;
+
+        foreach (int node in myLList)
+        {
+            if (index == n)
+            {
+                return node;
+            }
+            index++;
+        }
+        return 0;
+    }
+}
