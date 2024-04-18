f749d0f8f1764de17fc99e95af355e54e6d5c399
hendrickl <h.lincertin@gmail.com>
2024-04-18 10:27:09 +0200

    Task 11

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        string printedLine = "\\";
+
+        if (length <= 0)
+        {
+            Console.WriteLine("");
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            Console.WriteLine(printedLine);
+            Console.Write(new string(' ', i + 1));
+        }
+    }
+}
