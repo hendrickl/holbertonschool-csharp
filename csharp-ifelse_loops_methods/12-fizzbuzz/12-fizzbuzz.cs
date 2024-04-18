8a50e6f78d6dba7e2b673f0bfaf15367a146c802
hendrickl <h.lincertin@gmail.com>
2024-04-18 11:05:52 +0200

    Task 12

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fizz = "Fizz";
+        string buzz = "Buzz";
+        string fizzBuzz = "FizzBuzz";
+
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.Write(fizzBuzz + " ");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.Write(buzz + " ");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.Write(fizz + " ");
+            }
+            else
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.WriteLine("");
+    }
+}
