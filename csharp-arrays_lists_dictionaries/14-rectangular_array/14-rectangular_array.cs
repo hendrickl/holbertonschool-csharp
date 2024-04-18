c7fad3c87d2ac928b70e66cdaa9726a600eeca02
hendrickl <h.lincertin@gmail.com>
2024-04-18 13:29:52 +0200

    Task 14

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] my2DArray = new int[5, 5];
+        my2DArray[2, 2] = 1;
+
+        for (int i = 0; i < my2DArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < my2DArray.GetLength(1); j++)
+            {
+                Console.Write(my2DArray[i, j] + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
