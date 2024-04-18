f6625ded9b6f04e15e14bea86566ae56ba4c790d
hendrickl <h.lincertin@gmail.com>
2024-04-18 15:19:03 +0200

    task bonus 18

---
+++ csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] jaggedArray = new int[3][];
+        jaggedArray[0] = new int[] { 0, 1, 2, 3 };
+        jaggedArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+        jaggedArray[2] = new int[] { 0, 1 };
+
+        for (int i = 0; i < jaggedArray.Length; i++)
+        {
+            for (int j = 0; j < jaggedArray[i].Length; j++)
+            {
+                Console.Write(jaggedArray[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
