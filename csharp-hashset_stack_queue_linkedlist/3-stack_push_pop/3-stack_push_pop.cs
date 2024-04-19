fb5047b8cd7d9be741a2e61e106e20a812ebda71
hendrickl <h.lincertin@gmail.com>
2024-04-19 10:10:30 +0200

    Task 3: Remove comments

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,14 +5,7 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        // Print the number of items in aStack
-        // Format: Number of items: <number>
-
         Console.WriteLine($"Number of items: {aStack.Count}");
-
-        // Print the item at the top of aStack without removing it
-        // Format: Top item: <item>
-        // If aStack is empty, print Stack is empty
 
         if (aStack.Count == 0)
         {
@@ -23,9 +16,6 @@ class MyStack
             Console.WriteLine($"Top item: {aStack.Peek()}");
         }
 
-        // Print if aStack contains a given item search
-        // Format: Stack contains <search>: <True / False>
-
         if (aStack.Contains(search))
         {
             Console.WriteLine($"Stack contains \"{search}\": True");
@@ -35,8 +25,6 @@ class MyStack
             Console.WriteLine($"Stack contains \"{search}\": False");
         }
 
-        // If aStack contains the given item search, remove all items up to and including search; otherwise, leave aStack as is
-
         if (aStack.Contains(search))
         {
             while (aStack.Contains(search))
@@ -45,11 +33,7 @@ class MyStack
             }
         }
 
-        // Add a new given item newItem to aStack
-
         aStack.Push(newItem);
-
-        // Return aStack
 
         return aStack;
     }
