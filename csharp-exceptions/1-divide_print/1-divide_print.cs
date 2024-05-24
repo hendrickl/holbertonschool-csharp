81ca95b650ed26840aea35ba1fc0a480c348ffb6
hendrickl <h.lincertin@gmail.com>
2024-05-24 15:34:56 +0200

    Task 1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            Console.WriteLine("{0} / 0 = 0", a);
+        }
+    }
+}
