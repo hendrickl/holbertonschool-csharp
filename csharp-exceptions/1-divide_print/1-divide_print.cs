5a7f9461c3afe27b0687f19ec073632fe797e783
hendrickl <h.lincertin@gmail.com>
2024-05-24 15:42:06 +0200

    Fix task 1

--- csharp-exceptions/1-divide_print/1-divide_print.cs
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -4,14 +4,20 @@ class Int
 {
     public static void divide(int a, int b)
     {
+        int result = 0;
+
         try
         {
-            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+            result = a / b;
         }
         catch (DivideByZeroException)
         {
             Console.WriteLine("Cannot divide by zero");
             Console.WriteLine("{0} / 0 = 0", a);
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
         }
     }
 }
