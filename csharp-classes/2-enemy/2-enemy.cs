1d88cb82257b484769148ba04d0b975bc011c58a
hendrickl <h.lincertin@gmail.com>
2024-05-30 15:56:09 +0200

    Task 2: Classes & Namespaces

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a blueprint for a zombie.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// The health of the zombie.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class with a specified health value.
+        /// </summary>
+        /// <param name="value">The health of the zombie. Must be greater than or equal to 0.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="value"/> is less than 0.</exception>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+    }
+}
