b967e6394200da46b78ef0de5990376ceaa3b07f
hendrickl <h.lincertin@gmail.com>
2024-05-30 15:37:08 +0200

    Task 1: Classes & Namespaces

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a blueprint for a zombie.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// The health of the zombie.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        /// <param name="health">The health of the zombie. Default is 0.</param>
+        public Zombie(int health = 0)
+        {
+            this.health = health;
+        }
+    }
+}
