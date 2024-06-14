a125488ab5bc0171d99dc5f67279cd1af8b7653b
hendrickl <h.lincertin@gmail.com>
2024-06-14 12:44:40 +0200

    Fixed issue

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -4,9 +4,12 @@ class Obj
 {
     public static bool IsOfTypeInt(object obj)
     {
+        /*
         if (obj is int)
             return true;
         else
             return false;
+            */
+        return obj is int;
     }
 }
