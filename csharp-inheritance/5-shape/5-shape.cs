c6fb478415abacd843fc9e0a73d16a4b743602c3
hendrickl <h.lincertin@gmail.com>
2024-06-14 15:54:45 +0200

    Task 5: Inheritance

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Represents a geometric shape
+class Shape
+{
+    /// <summary>
+    /// Calculates the area of the shape.
+    /// </summary>
+    /// <returns>The area of the shape.</returns>
+    /// <exception cref="NotImplementedException">Thrown when the method is not implemented in a derived class.</exception>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
