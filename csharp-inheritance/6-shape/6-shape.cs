9df6ea4fd2ac4b66f311df8b6c8737d0c369899a
hendrickl <h.lincertin@gmail.com>
2024-06-14 16:09:15 +0200

    Task 6: Inheritance

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+// Represents a geometric shape
+class Shape
+{
+    /// <summary>
+    /// Calculates the area of the shape.
+    /// </summary>
+    /// <returns>The area of the shape.</returns>
+    /// <exception cref="NotImplementedException">Thrown when the method is not implemented in a derived class.</exception>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+// Represents a rectangle shape
+class Rectangle : Shape
+{
+    // The width of the rectangle
+    private int width;
+
+    // The height of the rectangle
+    private int height;
+
+    /// <summary>
+    /// Gets or sets the width of the rectangle.
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the width is set to a value less than 0.</exception>
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (width < 0)
+                throw new ArgumentOutOfRangeException("Width must be greater than or equal to 0");
+            width = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the height of the rectangle.
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the height is set to a value less than 0.</exception>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (height < 0)
+                throw new ArgumentOutOfRangeException("Height must be greater than or equal to 0");
+            height = value;
+        }
+    }
+}
