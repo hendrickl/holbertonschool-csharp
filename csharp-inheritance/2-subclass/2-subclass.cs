2347077f83a5383ffb7df50552654634aa3f4ea4
hendrickl <h.lincertin@gmail.com>
2024-06-14 13:49:18 +0200

    Fix dumb issue

--- csharp-inheritance/2-subclass/2-subclass.cs
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -10,7 +10,6 @@ class Obj
     /// <param name="baseType">The base type to check against.</param>
     /// <returns><c>true</c> if <paramref name="derivedType"/> is a subclass of <paramref name="baseType"/> and not the same as <paramref name="baseType"/>; otherwise, <c>false</c>.</returns>
     public static bool IsOnlyASubclass(Type derivedType, Type baseType)
-    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
     {
         return derivedType.IsSubclassOf(baseType) && derivedType != baseType;
     }
