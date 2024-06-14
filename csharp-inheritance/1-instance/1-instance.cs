db86fed7356a069bb76e1678f9aae8c5d45e68ed
hendrickl <h.lincertin@gmail.com>
2024-06-14 13:38:02 +0200

    Task 1: Inheritance

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Provides utility methods for object type checking.
+class Obj
+{
+    /// <summary>
+    /// Determines whether the specified object is an instance of an array.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns><c>true</c> if the object is an instance of an array; otherwise, <c>false</c>.</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj.GetType() == typeof(Array) || obj is Array)
+            return true;
+        else
+            return false;
+    }
+}
