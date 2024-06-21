64138c5023721ffdee45c8586fe8875d77448b6d
hendrickl <h.lincertin@gmail.com>
2024-06-21 15:13:21 +0200

    Task 1: TDD

---
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using System;
+using MyMath;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Divide_WhenNumIsZero_ShouldReturnNull()
+        {
+            int[,] matrix = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+            int num = 0;
+
+            int[,] result = Matrix.Divide(matrix, num);
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void Divide_WhenMatrixIsNull_ShouldReturnNull()
+        {
+            int[,] matrix = null;
+            int num = 2;
+
+            int[,] result = Matrix.Divide(matrix, num);
+
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void Divide_WhenMatrixIsNotNull_ShouldReturnResult()
+        {
+            int[,] matrix = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+            int num = 2;
+
+            int[,] result = Matrix.Divide(matrix, num);
+
+            Assert.AreEqual(1, result[0, 0]);
+            Assert.AreEqual(2, result[0, 1]);
+            Assert.AreEqual(3, result[1, 0]);
+            Assert.AreEqual(4, result[1, 1]);
+        }
+    }
+}
---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Provides operations for matrices.
+    /// </summary>
+    public class Matrix
+    {
+        /// <summary>
+        /// Divides each element of the matrix by the specified number.
+        /// </summary>
+        /// <param name="matrix">The matrix to be divided.</param>
+        /// <param name="num">The number by which each element of the matrix will be divided.</param>
+        /// <returns>A new matrix with each element divided by the specified number, or <c>null</c> if the input matrix is <c>null</c> or if <paramref name="num"/> is 0.</returns>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix == null)
+            {
+                return null;
+            }
+
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int[,] result = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    try
+                    {
+                        result[i, j] = matrix[i, j] / num;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Num cannot be 0");
+                        return null;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
